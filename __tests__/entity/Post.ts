51cd84e4b39871af4932c70f7e7875fa320435be
Bryan Müller <bmuller0975@gmail.com>
2019-08-11 18:27:22 -0600

    Add tests for pagintation

--- __tests__/entity/Post.ts
+++ __tests__/entity/Post.ts
@@ -1,10 +1,37 @@
-import { GraphQLID, GraphQLResolveInfo, GraphQLString } from "graphql";
+import {
+  GraphQLBoolean,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLResolveInfo,
+  GraphQLString
+} from "graphql";
 import { Entity, Column, ManyToOne, JoinColumn } from "typeorm";
 import { GraphQLDatabaseLoader } from "../../src";
-import { builder } from "../schema/index";
+import { builder } from "../schema";
 
 import { Node } from "./Node";
 import { User } from "./User";
+
+@builder.input()
+class PostPagination {
+  @builder.field(GraphQLInt)
+  offset!: number;
+
+  @builder.field(GraphQLInt)
+  limit!: number;
+}
+
+@builder.type()
+class PaginatedPost {
+  @builder.field(GraphQLBoolean)
+  hasMore!: boolean;
+
+  @builder.field(GraphQLInt)
+  offset!: number;
+
+  @builder.list(() => Post)
+  posts!: Post[];
+}
 
 @builder.input()
 class PostInput {
@@ -70,5 +97,49 @@ export class Post extends Node {
     info: GraphQLResolveInfo
   ) {
     return context.loader.loadMany(Post, args.where, info);
+  }
+
+  @builder.query({
+    returnType: {
+      type: () => PaginatedPost,
+      list: false,
+      nonNull: true
+    },
+    args: {
+      where: {
+        type: () => PostInput,
+        defaultValue: {}
+      },
+      pagination: {
+        type: () => PostPagination,
+        defaultValue: {}
+      }
+    }
+  })
+  async paginatedPosts(
+    rootValue: any,
+    args: any,
+    context: { loader: GraphQLDatabaseLoader },
+    info: GraphQLResolveInfo
+  ) {
+    const [posts, totalCount] = await context.loader.loadManyPaginated(
+      Post,
+      args.where,
+      info,
+      args.pagination
+    );
+    // Note: this is just an example of a simple pagination system that does
+    // not check if the end of the list is reached when calculating the next offset
+    // It is advised that a more robust/standardized implementation is used. As
+    // The actual pagination system is not part of the package yet, I do not feel like
+    // a robust offset calculation is relevant to testing the actual functionality which
+    // revolves around whether or not the paginated method returns the records with the correct
+    // limits and offset
+    const { offset, limit } = args.pagination;
+    return {
+      hasMore: offset + limit < totalCount,
+      offset: offset + limit,
+      posts
+    };
   }
 }
