64f3a0773e5e6071851f47b2854c755c2bfdacd1
Bryan Müller <bmuller0975@gmail.com>
2020-09-12 14:11:46 -0600

    test(Decorators): Add test decorator context

---
+++ src/__tests__/util/DecoratorContext.ts
@@ -0,0 +1,8 @@
+export interface DecoratorContext {
+  requireField: boolean;
+  requireEmbed: boolean;
+  requireRelation: boolean;
+  ignoreField: boolean;
+  ignoreEmbed: boolean;
+  ignoreRelation: boolean;
+}
